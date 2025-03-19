7b398cbe8bb01ee96bf829019c67b03e3324ca72
justjo3l <comedyandcomputers@gmail.com>
2025-03-19 11:34:17 +1100

    Remove header changes

--- lambda_functions/uploadRawCSV/lambda_function.py
+++ lambda_functions/uploadRawCSV/lambda_function.py
@@ -74,10 +74,6 @@ def lambda_handler(event, context):
             "statusCode": 200,
             "headers": {
                 "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
-                "Access-Control-Allow-Headers":
-                "Content-Type, Authorization, file, bucket",
             },
             "body": json.dumps({"URL": presigned_url})
         }
